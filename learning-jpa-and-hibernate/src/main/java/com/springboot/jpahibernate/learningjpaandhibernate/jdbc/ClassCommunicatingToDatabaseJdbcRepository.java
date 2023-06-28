d685c2040aad586426ea44b72c79554f7b2ffaf6
JoseSagwe <Josephsagwe421@gmail.com>
2023-06-28 09:53:39 +0300

    fifth commit

--- learning-jpa-and-hibernate/src/main/java/com/springboot/jpahibernate/learningjpaandhibernate/jdbc/ClassCommunicatingToDatabaseJdbcRepository.java
+++ learning-jpa-and-hibernate/src/main/java/com/springboot/jpahibernate/learningjpaandhibernate/jdbc/ClassCommunicatingToDatabaseJdbcRepository.java
@@ -1,8 +1,11 @@
 package com.springboot.jpahibernate.learningjpaandhibernate.jdbc;
 
 
+import com.springboot.jpahibernate.learningjpaandhibernate.Course.CourseSpecificationsClass;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -19,12 +22,37 @@ public class ClassCommunicatingToDatabaseJdbcRepository {
 
     private static String INSERT_QUERY =
             """                
-                    INSERT INTO course(id, name, author) VALUES (5, 'CyberSecurity', 'Developer Dante');
+                    INSERT INTO course(id, name, author) VALUES (? , ?, ?);
                                         
                     """;
 
-    public void insert(){
-        jdbcTemplate.update(INSERT_QUERY);
+    private static String DELETE_QUERY =
+            """                
+                    DELETE FROM course WHERE id=?;
+                                        
+                    """;
+
+    private static String SELECT_QUERY =
+            """                
+                    SELECT * FROM course WHERE id=?;
+                                        
+                    """;
+
+
+
+    public void insert(CourseSpecificationsClass course){
+        jdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(),course.getAuthor());
     }
+
+    public void deleteById(long id){
+        jdbcTemplate.update(DELETE_QUERY,id);
+    }
+
+    public CourseSpecificationsClass findById(long id){
+        //Result => Bean => Row Mapper
+        return jdbcTemplate.queryForObject(SELECT_QUERY, new BeanPropertyRowMapper<>(CourseSpecificationsClass.class), id);
+    }
+
+
 
 }
--- learning-jpa-and-hibernate/src/main/java/com/springboot/jpahibernate/learningjpaandhibernate/jdbc/ClassToFireQueryOnApplicationStart.java
+++ learning-jpa-and-hibernate/src/main/java/com/springboot/jpahibernate/learningjpaandhibernate/jdbc/ClassToFireQueryOnApplicationStart.java
@@ -1,5 +1,6 @@
 package com.springboot.jpahibernate.learningjpaandhibernate.jdbc;
 
+import com.springboot.jpahibernate.learningjpaandhibernate.Course.CourseSpecificationsClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,17 +9,29 @@ import org.springframework.stereotype.Component;
 public class ClassToFireQueryOnApplicationStart implements CommandLineRunner {
     //CommandLineRunner Is aon Interface used to run a logic on Application Start
 
-
     ClassCommunicatingToDatabaseJdbcRepository repository;
 
-    @Autowired
+  @Autowired
     public ClassToFireQueryOnApplicationStart(ClassCommunicatingToDatabaseJdbcRepository repository) {
         this.repository = repository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new CourseSpecificationsClass(1,"Java","Navin2"));
+        repository.insert(new CourseSpecificationsClass(2,"Kotlin","Navin"));
+        repository.insert(new CourseSpecificationsClass(3,"CyberSecurity","Navin Reddy"));
+        repository.insert(new CourseSpecificationsClass(4,"Spring Boot","Navin3"));
+        repository.insert(new CourseSpecificationsClass(5,"Android","Navin1000"));
+
+        //Delete Query
+        repository.deleteById(5);
+
+        //select Query
+        System.out.println( repository.findById(1));
+        System.out.println(repository.findById(2));
+        System.out.println(repository.findById(3));
+        System.out.println(repository.findById(4));
     }
 
 
