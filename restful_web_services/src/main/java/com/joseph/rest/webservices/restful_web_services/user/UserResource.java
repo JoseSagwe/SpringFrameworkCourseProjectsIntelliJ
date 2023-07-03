6246921b6da8d15dc358e279883809e36f738a39
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-03 13:04:41 +0300

    tenth

---
+++ restful_web_services/src/main/java/com/joseph/rest/webservices/restful_web_services/user/UserResource.java
@@ -0,0 +1,62 @@
+package com.joseph.rest.webservices.restful_web_services.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDataAccessObjectService service;
+
+    @Autowired
+    public UserResource(UserDataAccessObjectService service) {
+        this.service = service;
+    }
+
+
+    //GET /users
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+
+    //GET /users
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        User user = service.findOne(id);
+        if(user==null)
+            throw new UserNotFoundException("id:"+id);
+        return user;
+
+    }
+
+    //POST /users
+//    @PostMapping("/users")
+//    public void addUser(@RequestBody User user){
+//        service.saveUser(user);
+//    }
+
+
+    //Adding Response type e.g. 201 and location e.g. localhost:8080/users/5
+    //POST /users
+
+    @PostMapping("/users")
+    public ResponseEntity<User> addUserWithResponse(@RequestBody User user){
+
+        User savedUser = service.saveUser(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+}
