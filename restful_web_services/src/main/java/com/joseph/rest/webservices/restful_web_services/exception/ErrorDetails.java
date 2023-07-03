e83d9a29cddba9e1db7798b61c623c240fb78865
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-03 14:18:26 +0300

    tenth commit

---
+++ restful_web_services/src/main/java/com/joseph/rest/webservices/restful_web_services/exception/ErrorDetails.java
@@ -0,0 +1,33 @@
+package com.joseph.rest.webservices.restful_web_services.exception;
+
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.time.LocalDateTime;
+
+//Custom exception Structure
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(LocalDateTime timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+}
