900e9672b152386fa15a329ab0116c1f650f5121
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-06 10:35:05 +0300

    T

--- restful_web_services/src/main/java/com/joseph/rest/webservices/restful_web_services/calculator/Calculator.java
+++ restful_web_services/src/main/java/com/joseph/rest/webservices/restful_web_services/calculator/Calculator.java
@@ -1,4 +1,40 @@
 package com.joseph.rest.webservices.restful_web_services.calculator;
 
+import org.springframework.stereotype.Component;
+
 public class Calculator {
+    private int a;
+    private int b;
+
+    public Calculator() {
+    }
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "Calculator{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
+    }
 }
---
+++ restful_web_services/src/main/java/com/joseph/rest/webservices/restful_web_services/calculator/OperationsClass.java
@@ -0,0 +1,4 @@
+package com.joseph.rest.webservices.restful_web_services.calculator;
+
+public class OperationsClass {
+}
