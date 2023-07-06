ed2ace1f944400e9164aba4a64c40e5f54f3a127
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-06 10:39:38 +0300

    T

--- restful_web_services/src/main/java/com/joseph/rest/webservices/restful_web_services/calculator/OperationsClass.java
+++ restful_web_services/src/main/java/com/joseph/rest/webservices/restful_web_services/calculator/OperationsClass.java
@@ -1,4 +1,11 @@
 package com.joseph.rest.webservices.restful_web_services.calculator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OperationsClass {
+    public int addition(int a, int b){
+
+    }
 }
