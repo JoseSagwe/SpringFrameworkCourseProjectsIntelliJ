f127634c739b26f923b4c9975c1969e316c63c39
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-26 13:48:56 +0300

    W

--- spring-security/src/main/java/com/joseph/springsecurity/resources/TodoResource.java
+++ spring-security/src/main/java/com/joseph/springsecurity/resources/TodoResource.java
@@ -1,11 +1,15 @@
 package com.joseph.springsecurity.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class TodoResource {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     public static final List<Todo> LIST_TODOS = List.of(new Todo("Joseph", "Learn Ethical Hacking"),
                                                         new Todo("Joseph", "Learn Cyber security"));
@@ -23,7 +27,7 @@ public class TodoResource {
 
     @PostMapping ("/users/{username}/todos")
     public void createTodoForASpecificUser(@PathVariable String username, @RequestBody Todo todo){
-       
+        logger.info("Create {} for {}" , todo , username);
     }
 }
 
