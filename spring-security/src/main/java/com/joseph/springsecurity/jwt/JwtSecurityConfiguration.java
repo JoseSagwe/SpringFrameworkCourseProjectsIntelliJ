c9be9d305f1d5c21b892bf713df778438f75407c
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-27 17:32:05 +0300

    T

--- spring-security/src/main/java/com/joseph/springsecurity/jwt/JwtSecurityConfiguration.java
+++ spring-security/src/main/java/com/joseph/springsecurity/jwt/JwtSecurityConfiguration.java
@@ -1,7 +1,6 @@
-package com.joseph.springsecurity.basicAuthentication;
+package com.joseph.springsecurity.jwt;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,7 +9,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -19,7 +17,7 @@ import javax.sql.DataSource;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 //@Configuration
-public class BasicAuthSecurityConfiguration {
+public class JwtSecurityConfiguration {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
