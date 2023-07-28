a49db2f20cc7a189736f6174d53a03e592e87cf2
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-28 14:11:36 +0300

    F

--- Spring-OAuth/src/main/java/com/joseph/OAuth/HelloWorldResource.java
+++ Spring-OAuth/src/main/java/com/joseph/OAuth/HelloWorldResource.java
@@ -1,5 +1,12 @@
 package com.joseph.OAuth;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HelloWorldResource {
-    
+    @GetMapping("/")
+    public String helloWorld(Authentication authentication) {
+        System.out.println(authentication);
+        System.out.println(authentication.getPrincipal());
+        return "Hello World";
+    }
 }
--- Spring-OAuth/src/main/java/com/joseph/OAuth/OauthSecurityConfiguration.java
+++ Spring-OAuth/src/main/java/com/joseph/OAuth/OauthSecurityConfiguration.java
@@ -1,6 +1,12 @@
 package com.joseph.OAuth;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class OauthSecurityConfiguration {
