e19c74b76dd6f4f5adffa1d292dbb7484abbdf4a
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-29 15:06:31 +0300

    S

--- Learning-spring-maven/src/main/java/com/joseph/Learnmaven/LearnMavenApplication.java
+++ Learning-spring-maven/src/main/java/com/joseph/Learnmaven/LearnMavenApplication.java
@@ -1,10 +1,14 @@
 package com.joseph.Learnmaven;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearnMavenApplication {
+
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnMavenApplication.class, args);
