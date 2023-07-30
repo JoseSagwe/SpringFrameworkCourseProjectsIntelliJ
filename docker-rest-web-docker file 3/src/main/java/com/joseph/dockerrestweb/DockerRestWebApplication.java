0ffeaef2f73ebe2166c90e4beee9eea7af35507c
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-30 23:43:11 +0300

    S

---
+++ docker-rest-web-docker file 3/src/main/java/com/joseph/dockerrestweb/DockerRestWebApplication.java
@@ -0,0 +1,13 @@
+package com.joseph.dockerrestweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerRestWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerRestWebApplication.class, args);
+	}
+
+}
