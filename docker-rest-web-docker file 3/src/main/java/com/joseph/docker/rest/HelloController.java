ffaf9c1f96f28b98ee5fe01774a0653f0d9dd6ed
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-31 21:06:28 +0300

    M

--- docker-rest-web-docker file 3/src/main/java/com/joseph/docker/rest/HelloController.java
+++ docker-rest-web-docker file 3/src/main/java/com/joseph/docker/rest/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "{\"message\":\"Hello Developer Joseph \"}";
+        return "{\"message\":\"Hello Developer Joseph Sagwe Birisio \"}";
 //        return "{\"message\":\"Hello World Java v1\"}";
     }
 }
