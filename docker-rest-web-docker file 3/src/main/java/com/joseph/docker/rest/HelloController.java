e48a874a60d38f8faa0d28d048c55e0ec8e19803
JoseSagwe <Josephsagwe421@gmail.com>
2023-07-31 21:12:03 +0300

    M

--- docker-rest-web-docker file 3/src/main/java/com/joseph/docker/rest/HelloController.java
+++ docker-rest-web-docker file 3/src/main/java/com/joseph/docker/rest/HelloController.java
@@ -3,12 +3,14 @@ package com.joseph.docker.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloController {
 
+
     @GetMapping("/hello")
     public String hello(){
-        return "{\"message\":\"Hello Developer Joseph  \"}";
+        return "{\"message\":\"Hello Developer Sagw  \"}";
 //        return "{\"message\":\"Hello World Java v1\"}";
     }
 }
