034ac0e561022f37de933141fa30883700d4c6d6
JoseSagwe <Josephsagwe421@gmail.com>
2023-08-01 22:22:24 +0300

    T

--- docker-rest-web-docker file 3/src/main/java/com/joseph/docker/rest/HelloController.java
+++ docker-rest-web-docker file 3/src/main/java/com/joseph/docker/rest/HelloController.java
@@ -3,10 +3,8 @@ package com.joseph.docker.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class HelloController {
-
 
     @GetMapping("/hello")
     public String hello(){
--- docker-rest-web-docker file 3/src/test/java/com/joseph/docker/rest/DockerRestWebApplicationTests.java
+++ docker-rest-web-docker file 3/src/test/java/com/joseph/docker/rest/DockerRestWebApplicationTests.java
@@ -3,7 +3,6 @@ package com.joseph.docker.rest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 @SpringBootTest
 class DockerRestWebApplicationTests {
 
