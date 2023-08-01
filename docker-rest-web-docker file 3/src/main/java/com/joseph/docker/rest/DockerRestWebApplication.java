e0635a73ee648379c9111e0f157de61e5ee0d553
JoseSagwe <Josephsagwe421@gmail.com>
2023-08-01 22:27:34 +0300

    T

--- docker-rest-web-docker file 3/src/main/java/com/joseph/docker/rest/DockerRestWebApplication.java
+++ docker-rest-web-docker file 3/src/main/java/com/joseph/docker/rest/DockerRestWebApplication.java
@@ -2,11 +2,9 @@ package com.joseph.docker.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class DockerRestWebApplication {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(DockerRestWebApplication.class, args);
 	}
--- docker-rest-web-docker file 3/src/test/java/com/joseph/docker/rest/DockerRestWebApplicationTests.java
+++ docker-rest-web-docker file 3/src/test/java/com/joseph/docker/rest/DockerRestWebApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DockerRestWebApplicationTests {
 
+
 	@Test
 	void contextLoads() {
 	}
