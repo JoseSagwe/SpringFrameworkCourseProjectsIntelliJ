57a60d31c9a00c64d0e01dc8c8e1548b996df4a0
JoseSagwe <Josephsagwe421@gmail.com>
2023-08-01 22:13:48 +0300

    T

--- docker-rest-web-docker file 3/src/main/java/com/joseph/docker/rest/HelloController.java
+++ docker-rest-web-docker file 3/src/main/java/com/joseph/docker/rest/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "{\"message\":\"Hello Developer Sagw  \"}";
+        return "{\"message\":\"He \"}";
 //        return "{\"message\":\"Hello World Java v1\"}";
     }
 }
--- docker-rest-web-docker file 3/src/test/java/com/joseph/docker/rest/DockerRestWebApplicationTests.java
+++ docker-rest-web-docker file 3/src/test/java/com/joseph/docker/rest/DockerRestWebApplicationTests.java
@@ -3,6 +3,7 @@ package com.joseph.docker.rest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class DockerRestWebApplicationTests {
 
