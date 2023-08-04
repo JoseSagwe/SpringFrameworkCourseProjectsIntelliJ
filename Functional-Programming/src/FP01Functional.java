fbc5a7939dcc62f0ca4796f13c558cebc4a04c58
JoseSagwe <Josephsagwe421@gmail.com>
2023-08-04 12:31:00 +0300

    F

--- Functional-Programming/src/FP01Functional.java
+++ Functional-Programming/src/FP01Functional.java
@@ -3,16 +3,24 @@ import java.util.List;
 public class FP01Functional {
     public static void main(String[] args) {
 
-        ListOfNumbersFunctional(List.of(12,15,67,62,54,98));
+        ListOfNumbersFunctional(List.of(122,156,687,672,540,988));
 
     }
 
-    private static  void print(int number){
-        System.out.println(number);
-    }
+//    private static  void print(int number){
+//        System.out.println(number);
+//    }
 
     private static void ListOfNumbersFunctional(List<Integer> numbers) {
         numbers.stream()
-                .forEach(FP01Functional::print);
+//              .forEach(FP01Functional::print);
+                .forEach(System.out::println);
     }
+
+    private static void EvenNumbersFunctional(List<Integer> numbers) {
+        numbers.stream()
+//
+                .forEach(System.out::println);
+    }
+
 }
--- Functional-Programming/src/FP01Structured.java
+++ Functional-Programming/src/FP01Structured.java
@@ -3,13 +3,22 @@ import java.util.List;
 public class FP01Structured {
     public static void main(String[] args) {
 
-        ListOfNumbersStructured(List.of(12,15,67,62,54,98));
+//        ListOfNumbersStructured(List.of(12,15,67,62,54,98));
+        EvenListOfNumbersStructured(List.of(23,152,674,62,44,98));
 
     }
 
     private static void ListOfNumbersStructured(List<Integer> numbers) {
         for(int number:numbers){
             System.out.println(number);
+        }
+    }
+
+    private static void EvenListOfNumbersStructured(List<Integer> numbers) {
+        for(int number:numbers) {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
         }
     }
 }
